ad510136c0dd60f230e91b56935d99a4f8d09237
Svetlana Zlydneva <svetlanaderiglazova87@gmail.com>
2021-02-23 13:48:19 +0200

    get all orders

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -72,15 +72,13 @@ class OrderController {
   //     }
   //   }
 
-  async getOrders(_, res, next) {
+  async getOrders(req, res, next) {
     try {
       const orders = await orderModel
-        .find()
-        .where("author")
-        .equals(req.user.login)
-        .where("status")
-        .ne("rejected")
-        .sort({ number: -1 });
+        .find({
+          $and: [{ author: req.user.login }, { status: { $ne: "processed" } }],
+        })
+        .sort({ number: 1 });
       if (orders.length === 0) throw new NotFoundError("Not found Data");
       return res.status(200).json(orders);
     } catch (error) {
